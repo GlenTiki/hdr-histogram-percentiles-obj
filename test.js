37e1e12c49a05f3ea39f1968c10c5e28615b885a
Glen Keane <glenkeane.94@gmail.com>
2016-07-15 14:29:38 +0100

    updated tests

--- test.js
+++ test.js
@@ -35,3 +35,33 @@ test('should return a valid hist as object', (t) => {
   t.type(withPercentiles.p9999, 'number')
   t.type(withPercentiles.p99999, 'number')
 })
+
+test('should return expected numbers', (t) => {
+  t.plan(16)
+  const histogram = new Histogram(1, 10)
+
+  histogram.record(4)
+  histogram.record(5)
+  histogram.record(6)
+
+  // any of the numbers below _could_ be 0, so we do a type test instead of t.ok
+  const result = histPercentileObj.histAsObj(histogram, 15)
+  t.ok(result)
+  t.equal(result.average, 5)
+  t.equal(result.mean, 5)
+  t.equal(result.stddev, 0.82)
+  t.equal(result.min, 4)
+  t.equal(result.max, 6)
+  t.equal(result.total, 15)
+
+  const withPercentiles = histPercentileObj.addPercentiles(histogram, result)
+  t.ok(withPercentiles)
+  t.equal(withPercentiles.average, 5)
+  t.equal(withPercentiles.p50, 5)
+  t.equal(withPercentiles.p75, 5)
+  t.equal(withPercentiles.p90, 6)
+  t.equal(withPercentiles.p99, 6)
+  t.equal(withPercentiles.p999, 6)
+  t.equal(withPercentiles.p9999, 6)
+  t.equal(withPercentiles.p99999, 6)
+})
